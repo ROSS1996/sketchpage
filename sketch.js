65791335cd91b3d7bb429ddb6440b122a9c18dbb
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-10-21 10:27:48 -0300

    Add mouse button state

--- sketch.js
+++ sketch.js
@@ -1,26 +1,50 @@
-const container = document.getElementById("drawing");
 
 const pixels = 32;
+
+const container = document.getElementById("drawing");
+
+var mousePressed = false;
+
+container.addEventListener('mouseup', function () {
+    console.log('Mouse unpressed')
+    mousePressed = false;
+})
+
+container.addEventListener('mousedown', function () {
+    console.log('Mouse pressed')
+    mousePressed = true;
+})
 
 for (i = 0; i < pixels; i++) {
     const pixel = document.createElement("div")
     pixel.classList.add("defPixel")
     pixel.id = `pixel${i}`;
+    pixel.draggable = false;
     container.appendChild(pixel)
     pixel.addEventListener("click", function () {
         changeColor(this.id)
     })
+    pixel.addEventListener('mouseenter', function () {
+        if (mousePressed == true) {
+            changeColor(this.id)
+        }
+    })
 }
 
+
+
 function changeColor(element) {
+
     const pixel = document.getElementById(element);
     const style = window.getComputedStyle(pixel)
-    const bgColor = style.getPropertyValue('background-color');
-    console.log(`BG Color: ${bgColor}`)
-    if (bgColor == 'rgb(255, 255, 255)' || bgColor == 'rgb(128, 128, 128)') {
+    const cssColor = style.getPropertyValue('background-color');
+    const inlineColor = pixel.style.backgroundColor
+    //console.log(`BG Color: ${bgColor}`)
+    if (cssColor == 'rgb(255, 255, 255)' || inlineColor == 'white' || inlineColor == '') {
         console.log('BG Color white')
         document.getElementById(element).style.backgroundColor = 'black'
-    } else {
+    }
+    else if (cssColor == 'rgb(255, 255, 255)' || inlineColor == 'black') {
         console.log('BG Color Black')
         document.getElementById(element).style.backgroundColor = 'white'
     }
